7824c90e2b3a5544ebf0137cefed627c08ffa2b6
rahandi <rahandinoor@gmail.com>
2019-11-14 21:36:48 +0700

    auth with roles, CRUD role not implemented yet

---
+++ pkkatering/database/seeds/RoleTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Role;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_admin = new Role();
+        $role_admin->name = 'admin';
+        $role_admin->description = 'administrator';
+        $role_admin->save();
+    }
+}
